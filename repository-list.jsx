db18e53131cfbc006b265b1bc442c4610e9c79b9
OoogleBoogle <simattfield@gmail.com>
2016-05-17 07:29:10 -0700

    Initial Commit....already half way through

---
+++ repository-list.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+
+var Repository = require('./repository');
+
+var RepositoryList = React.createClass({
+  addRepository: function() {
+    var repositoryName = this.refs.repositoryName.value;
+    // TODO: add repo to state
+  },
+
+  render: function() {
+    var repositories = this.props.repositories.map(function(repository) {
+      return <Repository repository={repository} key={repository.name} />;
+    });
+    return (
+      <div className="repository-list">
+        {repositories}
+        <input type="text" ref="repositoryName" />
+        <button type="button" onClick={this.addRepository}>Add Repo</button>
+      </div>
+    );
+  }
+});
+
+module.exports = RepositoryList;
